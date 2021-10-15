22fbaa3b64c8fc497ac33bf60302ff9bd4d71ac8
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-10-16 09:45:20 +1300

    Added soft reset, heater on and off

--- SensirionSHT20/Sht20.cs
+++ SensirionSHT20/Sht20.cs
@@ -1,5 +1,5 @@
 ﻿//---------------------------------------------------------------------------------
-// Copyright (c) September 2021, devMobile Software
+// Copyright (c) October 2021, devMobile Software
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,11 +24,15 @@ namespace devMobile.IoT.NetCore.Sensirion
 
 	public class Sht20 : IDisposable
 	{
-		public const int DefaultI2cAddress = 0x40;
+		public const byte DefaultI2cAddress = 0x40;
 		private const byte UserRegisterRead = 0xE7;
-		private const byte UserRegisterWrite = 0xE6; // need this for heater and reset
+		private const byte UserRegisterWrite = 0xE6; 
+		private const byte HeaterOnMask = 0x04;
+		private const byte HeaterOffMask = 0xFb ;
+		private const byte ReadingWaitmSec = 70;
 		private const byte TemperatureNoHold = 0xF3;
-		private const byte HumidityNoHold = 0xf5;
+		private const byte HumidityNoHold = 0xF5;
+		private const byte SoftReset = 0xFE;
 
 		private I2cDevice _i2cDevice = null;
 
@@ -36,13 +40,13 @@ namespace devMobile.IoT.NetCore.Sensirion
 		{
 			byte[] writeBuffer = new byte[1] { UserRegisterRead };
 			byte[] readBuffer = new byte[1] { 0 };
-			_i2cDevice = i2cDevice ?? throw new ArgumentNullException(nameof(i2cDevice));
+			_i2cDevice = i2cDevice ?? throw new ArgumentNullException(nameof(_i2cDevice));
 
 			_i2cDevice.WriteRead(writeBuffer, readBuffer);
 
 			if (readBuffer[0] == 0)
 			{
-				throw new ApplicationException("GroveBaseHatRPI not found");
+				throw new ApplicationException("SHT20 device not found");
 			}
 		}
 
@@ -50,18 +54,21 @@ namespace devMobile.IoT.NetCore.Sensirion
 		public double Temperature()
 		{
 			byte[] readBuffer = new byte[3] { 0, 0, 0 };
-			_i2cDevice = _i2cDevice ?? throw new ArgumentNullException(nameof(_i2cDevice));
+			if (_i2cDevice == null)
+			{
+				throw new ArgumentNullException(nameof(_i2cDevice));
+			}
 
 			_i2cDevice.WriteByte(TemperatureNoHold);
 
-			Thread.Sleep(70);
+			Thread.Sleep(ReadingWaitmSec);
 
 			_i2cDevice.Read(readBuffer);
 
-			ushort rawTemperature = (ushort)(readBuffer[0] << 8);
-			rawTemperature += readBuffer[1];
+			ushort temperatureRaw = (ushort)(readBuffer[0] << 8);
+			temperatureRaw += readBuffer[1];
 
-			double temperature = rawTemperature * (175.72 / 65536.0) - 46.85; // 0.0026812744140625
+			double temperature = temperatureRaw * (175.72 / 65536.0) - 46.85;
 
 			return temperature;
 		}
@@ -69,48 +76,82 @@ namespace devMobile.IoT.NetCore.Sensirion
 		public double Humidity()
 		{
 			byte[] readBuffer = new byte[3] { 0, 0, 0 };
-			_i2cDevice = _i2cDevice ?? throw new ArgumentNullException(nameof(_i2cDevice));
+			if (_i2cDevice == null)
+			{
+				throw new ArgumentNullException(nameof(_i2cDevice));
+			}
 
 			_i2cDevice.WriteByte(HumidityNoHold);
 
-			Thread.Sleep(70);
+			Thread.Sleep(ReadingWaitmSec);
 
 			_i2cDevice.Read(readBuffer);
 
-			ushort rawTemperature = (ushort)(readBuffer[0] << 8);
-			rawTemperature += readBuffer[1];
+			ushort humidityRaw = (ushort)(readBuffer[0] << 8);
+			humidityRaw += readBuffer[1];
 
-
-			double humidity = rawTemperature * (125.0 / 65536.0) - 6.0;
+			double humidity = humidityRaw * (125.0 / 65536.0) - 6.0;
 
 			return humidity;
 		}
 
-		public bool Reset()
+		public void Reset()
 		{
-			/*
-  bool b = writeCmd(SHT2x_SOFT_RESET);
-  if (b == false) return false;
-  return true;
-			*/
-			return true;
+			if (_i2cDevice == null)
+			{
+				throw new ArgumentNullException(nameof(_i2cDevice));
+			}
+
+			_i2cDevice.WriteByte(SoftReset);
+
+			Thread.Sleep(70);
 		}
 
 		public void HeaterOn()
 		{
+			byte[] writeBufferRead = new byte[1] { UserRegisterRead };
+			byte[] readBuffer = new byte[1] { 0 };
+			if (_i2cDevice == null)
+			{
+				throw new ArgumentNullException(nameof(_i2cDevice));
+			}
 
+			_i2cDevice.WriteRead(writeBufferRead, readBuffer);
+
+			byte[] writeBufferWrite = new byte[2] { UserRegisterWrite, (byte)(readBuffer[0] | HeaterOnMask)};
+
+			_i2cDevice.Write(writeBufferWrite);
 		}
 
 		public bool IsHeaterOn()
 		{
-			return false;
+			byte[] writeBuffer = new byte[1] { UserRegisterRead };
+			byte[] readBuffer = new byte[1] { 0 };
+			if (_i2cDevice == null)
+			{
+				throw new ArgumentNullException(nameof(_i2cDevice));
+			}
+
+			_i2cDevice.WriteRead(writeBuffer, readBuffer);
+
+			return ((readBuffer[0] & HeaterOnMask) == HeaterOnMask);
 		}
 
 		public void HeaterOff()
 		{
+			byte[] writeBufferRead = new byte[1] { UserRegisterRead };
+			byte[] readBuffer = new byte[1] { 0 };
+			if (_i2cDevice == null)
+			{
+				throw new ArgumentNullException(nameof(_i2cDevice));
+			}
 
+			_i2cDevice.WriteRead(writeBufferRead, readBuffer);
+
+			byte[] writeBufferWrite = new byte[2] { UserRegisterWrite, (byte)(readBuffer[0] & HeaterOffMask) };
+
+			_i2cDevice.Write(writeBufferWrite);
 		}
-
 
 		public void Dispose()
 		{
